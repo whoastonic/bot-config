e49a233bd8f870e5e372303daf2c3b81e812ef37
Alex Taxiera <alex.taxiera@gmail.com>
2020-02-01 19:37:28 -0500

    try new database ish

--- bots/pg/scripts/05_READER.sh
+++ bots/pg/scripts/05_READER.sh
@@ -5,5 +5,5 @@ PASSW=idyllic-horses
 
 PGPASSWORD="$POSTGRES_PASSWORD" psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
 	CREATE USER ${USER} WITH ENCRYPTED PASSWORD '${PASSW}';
-	GRANT readonly TO external;
+	GRANT readonly TO ${USER};
 EOSQL
