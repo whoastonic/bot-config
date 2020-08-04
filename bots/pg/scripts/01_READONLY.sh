340dbd368cff77aa0b7ed28f9b74bbbb677585a7
Alex Taxiera <alex.taxiera@gmail.com>
2020-08-04 16:30:07 -0400

    use PG env for admin creds

--- bots/pg/scripts/01_READONLY.sh
+++ bots/pg/scripts/01_READONLY.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 set -e
 
-ADMIN_USER="$(< /run/secrets/ADMIN_USER)"
-ADMIN_PASS="$(< /run/secrets/ADMIN_PASS)"
-
 USER="$(< /run/secrets/READONLY_USER)"
 PASSW="$(< /run/secrets/READONLY_PASS)"
 
-PGPASSWORD="$ADMIN_PASS" psql -v ON_ERROR_STOP=1 --username "$ADMIN_USER" <<-EOSQL
+PGPASSWORD="$POSTGRES_PASSWORD" psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
   CREATE ROLE readonly;
 
   CREATE USER ${USER} WITH ENCRYPTED PASSWORD '${PASSW}';
